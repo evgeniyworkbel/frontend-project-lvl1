764b02d7bd3d65acceb66b19874dfb61f26090b3
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2022-04-30 00:56:56 +0300

    Create src/cli.js; Add request userName installing and using github rep "readlieSync"

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,2 +1,5 @@
 #!/usr/bin/env node
+import { userName } from "../src/cli.js";
+
 console.log('Welcome to the Brain Games!');
+console.log (`Hello, ${userName()}!`); 
---
+++ src/cli.js
@@ -0,0 +1,4 @@
+import readlineSync from "readline-sync";
+
+const userName = () => readlineSync.question('May I have your name? ');
+export { userName };
