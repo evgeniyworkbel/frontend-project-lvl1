24446f5b52de0a388a6932c387b31dc4e8ca19e9
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2022-05-19 22:51:44 +0300

    Move logic of greeting from brain-games.js to cli.js (commands in brain-games.js only invoke

--- bin/brain-games.js
+++ bin/brain-games.js
@@ -1,5 +1,4 @@
 #!/usr/bin/env node
-import userName from '../src/cli.js';
+import greeting from '../src/cli.js';
 
-console.log('Welcome to the Brain Games!');
-console.log(`Hello, ${userName()}!`);
+greeting();
--- src/cli.js
+++ src/cli.js
@@ -1,4 +1,9 @@
 import readlineSync from 'readline-sync';
 
 const userName = () => readlineSync.question('May I have your name? ');
-export default userName;
+const greeting = () => {
+  console.log('Welcome to the Brain Games!');
+  console.log(`Hello, ${userName()}!`);
+};
+
+export default greeting;
