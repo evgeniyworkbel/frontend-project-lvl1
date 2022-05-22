aed2a288f1843a9249c327929c3da4dd8ca3b101
Evgeniy Samuta <evgeniyworkbel@gmail.com>
2022-05-22 11:18:25 +0300

    Request username move to function "greeting()"

--- src/cli.js
+++ src/cli.js
@@ -1,9 +1,9 @@
 import readlineSync from 'readline-sync';
 
-const userName = () => readlineSync.question('May I have your name? ');
 const greeting = () => {
   console.log('Welcome to the Brain Games!');
-  console.log(`Hello, ${userName()}!`);
+  const userName = readlineSync.question('May I have your name? ');
+  console.log(`Hello, ${userName}!`);
 };
 
 export default greeting;
